8a77084e1aab15be65cc9fe3d768336b48247cea
Keshav919 <krungta@ucsd.edu>
2020-05-24 23:27:35 -0700

    Added extracting sample to dataloader

--- utils/datareader.py
+++ utils/datareader.py
@@ -1,26 +1,32 @@
+import sys
+sys.path.append('../')
+sys.path.append('../nuscenes_devkit/python_sdk/')
+
 import numpy as np
 import cv2
 import os
 import csv
 from torch.utils.data import Dataset
 import imageio
+from nuscenes_devkit.python_sdk.nuscenes import *
+from nuscenes.map_expansion.map_api import NuScenesMap
 
-
-data_path = "" #path to data stored
 
 class nuScenesdata(Dataset):
 
     def __init__(self):
 
-        self.data_dir = "" #path to data lists
-
-        self.input = "" #read all the data that could be inputted into the network
-        self.labels = "" #read all the labels that could be inputted into the network
+        data_path = "/home/krungta/ECE_285/data/sets/nuscenes/" #path to data stored
+        self.nusc = NuScenes(version='v1.0-mini', dataroot=data_path, verbose=True)
+#         nusc_map = NuScenesMap(dataroot=data_path, map_name='singapore-onenorth')
 
 
     def __len__(self):
-        return len(self.input) #return length of labels or input should be the same
+        return 3 #return length of labels or input should be the same
 
     def __getitem__(self, idx):
 
         #return an individual input into the network                                                
+        sample_token = self.nusc.sample[idx]['token']
+        return self.nusc.get('sample', sample_token)
+        
---
+++ utils/test_loader.py
@@ -0,0 +1,16 @@
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import cv2
+from datareader import nuScenesdata
+from nuscenes.map_expansion.map_api import NuScenesMap
+
+dataset = nuScenesdata()
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True, drop_last=True, num_workers=1)
+
+for batch, data in enumerate(dataloader):
+    print(batch, data)
+    break
