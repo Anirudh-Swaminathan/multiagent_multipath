98f0903f4b44763debf3bd39dedbbdcc857dd685
Anirudh Swaminathan <aniswami97@gmail.com>
2020-06-01 20:56:02 -0700

    Add final random betterment

--- toy_env/test.py
+++ toy_env/test.py
@@ -27,11 +27,14 @@ class IntersectionScene:
         self.closeness = 75
         self.vw = 10
         self.vh = 20
+        # distance from intersection minimum
+        self.iw = 60
+        self.ih = 60
 
         self.starting_points = list()
         # bottom lane => x in (intersection_bounds+vehicle_width/2, intersection_bounds-vehicle_width/2); y in (-(40+vehicle_length/2) to -(40 + closeness))
         sbot_posx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-        sbot_posy = np.random.uniform(self.intersection_bounds[0][1]-self.vh/2, self.intersection_bounds[0][1]-self.closeness)
+        sbot_posy = np.random.uniform(self.intersection_bounds[0][1]-self.ih/2, self.intersection_bounds[0][1]-self.closeness)
         sbot_pos = np.array([sbot_posx, sbot_posy])
         # velocity towards top
         sbot_vel = np.array([0, 1])
@@ -43,12 +46,12 @@ class IntersectionScene:
             sbot_gvel = np.copy(sbot_vel)
         elif sbot_int == TL:
             sbot_gvel = np.array([-1, 0])
-            sbot_golx = np.random.uniform(self.intersection_bounds[0][0]-self.vh/2, self.intersection_bounds[0][0]-self.closeness)
+            sbot_golx = np.random.uniform(self.intersection_bounds[0][0]-self.ih/2, self.intersection_bounds[0][0]-self.closeness)
             sbot_goly = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
             sbot_gol = np.array([sbot_golx, sbot_goly])
         elif sbot_int == TR:
             sbot_gvel = np.array([1, 0])
-            sbot_golx = np.random.uniform(self.intersection_bounds[1][0]+self.vh/2, self.intersection_bounds[1][0]+self.closeness)
+            sbot_golx = np.random.uniform(self.intersection_bounds[1][0]+self.ih/2, self.intersection_bounds[1][0]+self.closeness)
             sbot_goly = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
             sbot_gol = np.array([sbot_golx, sbot_goly])
         # append the entire state to a list
@@ -58,7 +61,7 @@ class IntersectionScene:
 
         # top lane => x in (intersection_bounds+vehicle_width/2, intersection_bounds-vehicle_width/2); y in ((40+vehicle_length/2), (40 + closeness))
         stop_posx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-        stop_posy = np.random.uniform(self.intersection_bounds[1][1]+self.vh/2, self.intersection_bounds[1][1]+self.closeness)
+        stop_posy = np.random.uniform(self.intersection_bounds[1][1]+self.ih/2, self.intersection_bounds[1][1]+self.closeness)
         stop_pos = np.array([stop_posx, stop_posy])
         # velocity towards bottom
         stop_vel = np.array([0, -1])
@@ -70,19 +73,19 @@ class IntersectionScene:
             stop_gol = np.array([stop_posx, stop_posy-self.closeness]) 
         elif stop_int == TL:
             stop_gvel = np.array([1, 0])
-            stop_golx = np.random.uniform(self.intersection_bounds[1][0]+self.vh/2, self.intersection_bounds[1][0]+self.closeness)
+            stop_golx = np.random.uniform(self.intersection_bounds[1][0]+self.ih/2, self.intersection_bounds[1][0]+self.closeness)
             stop_goly = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
             stop_gol = np.array([stop_golx, stop_goly])
         elif stop_int == TR:
             stop_gvel = np.array([-1, 0])
-            stop_golx = np.random.uniform(self.intersection_bounds[0][0]-self.vh/2, self.intersection_bounds[0][0]-self.closeness)
+            stop_golx = np.random.uniform(self.intersection_bounds[0][0]-self.ih/2, self.intersection_bounds[0][0]-self.closeness)
             stop_goly = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
             stop_gol = np.array([stop_golx, stop_goly])
         stop = tuple((stop_pos, stop_vel, stop_int, stop_gol, stop_gvel))
         self.starting_points.append(stop)
 
         # right lane => x in (intersection_bounds+vehicle_width/2, intersection_bounds+closeness); y in (-(40+vehicle_width/2), (40 + vehcile_width/2))
-        sr_posx = np.random.uniform(self.intersection_bounds[1][0]+self.vh/2, self.intersection_bounds[1][0]+self.closeness)
+        sr_posx = np.random.uniform(self.intersection_bounds[1][0]+self.ih/2, self.intersection_bounds[1][0]+self.closeness)
         sr_posy = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
         sr_pos = np.array([sr_posx, sr_posy])
         # velocity direction
@@ -96,18 +99,18 @@ class IntersectionScene:
         elif sr_int == TL:
             sr_gvel = np.array([0, -1])
             sr_golx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-            sr_goly = np.random.uniform(self.intersection_bounds[0][1]-self.vh/2, self.intersection_bounds[0][1]-self.closeness)
+            sr_goly = np.random.uniform(self.intersection_bounds[0][1]-self.ih/2, self.intersection_bounds[0][1]-self.closeness)
             sr_gol = np.array([sr_golx, sr_goly])
         elif sr_int == TR:
             sr_gvel = np.array([0, 1])
             sr_golx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-            sr_goly = np.random.uniform(self.intersection_bounds[1][1]+self.vh/2, self.intersection_bounds[1][1]+self.closeness)
+            sr_goly = np.random.uniform(self.intersection_bounds[1][1]+self.ih/2, self.intersection_bounds[1][1]+self.closeness)
             sr_gol = np.array([sr_golx, sr_goly])
         sr = tuple((sr_pos, sr_vel, sr_int, sr_gol, sr_gvel))
         self.starting_points.append(sr)
 
         # left lane => x in (-intersection_bounds-vehicle_width/2, -intersection_bounds-closeness); y in (-(40+vehicle_width/2), (40 + vehcile_width/2))
-        sl_posx = np.random.uniform(self.intersection_bounds[0][0]-self.vh/2, self.intersection_bounds[0][0]-self.closeness)
+        sl_posx = np.random.uniform(self.intersection_bounds[0][0]-self.ih/2, self.intersection_bounds[0][0]-self.closeness)
         sl_posy = np.random.uniform(self.intersection_bounds[0][1]+self.vw/2, self.intersection_bounds[1][1]-self.vw/2)
         sl_pos = np.array([sl_posx, sl_posy])
         # velocity direction
@@ -121,12 +124,12 @@ class IntersectionScene:
         elif sl_int == TL:
             sl_gvel = np.array([0, 1])
             sl_golx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-            sl_goly = np.random.uniform(self.intersection_bounds[1][1]+self.vh/2, self.intersection_bounds[1][1]+self.closeness)
+            sl_goly = np.random.uniform(self.intersection_bounds[1][1]+self.ih/2, self.intersection_bounds[1][1]+self.closeness)
             sl_gol = np.array([sl_golx, sl_goly])
         elif sl_int == TR:
             sl_gvel = np.array([0, -1])
             sl_golx = np.random.uniform(self.intersection_bounds[0][0]+self.vw/2, self.intersection_bounds[1][0]-self.vw/2)
-            sl_goly = np.random.uniform(self.intersection_bounds[0][1]-self.vh/2, self.intersection_bounds[0][1]-self.closeness)
+            sl_goly = np.random.uniform(self.intersection_bounds[0][1]-self.ih/2, self.intersection_bounds[0][1]-self.closeness)
             sl_gol = np.array([sl_golx, sl_goly])
             
         sl = tuple((sl_pos, sl_vel, sl_int, sl_gol, sl_gvel))
