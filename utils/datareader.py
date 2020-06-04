c1a954b9f4e426b41ca5439abe6458dbf25c4df7
skomatin <skomatin@ucsd.edu>
2020-06-03 20:35:02 -0700

    Cleaned up code, increased efficiency and started to fix data misalignment issue

--- utils/datareader.py
+++ utils/datareader.py
@@ -1,8 +1,8 @@
 import sys
 
 #comment if you used pip install else append the appropriate paths
-sys.path.append('../')
-sys.path.append('../nuscenes_devkit/python_sdk/')
+# sys.path.append('../')
+# sys.path.append('../nuscenes_devkit/python_sdk/')
 
 from collections import Counter
 from itertools import chain
@@ -30,32 +30,33 @@ from nuscenes.prediction.input_representation.interface import InputRepresentati
 from nuscenes.prediction.input_representation.combinators import Rasterizer
 
 DATA_PATH = "../data/sets/nuscenes/" #path to data stored
+DATA_VERSION = 'v1.0-mini'
 NUM_AGENTS = 2
-TRAJECTORY_TIME_INTERVAL = 6
-
+TRAJECTORY_TIME_INTERVAL = 6 #length (in time) per data point
 
 
 class nuScenesdata(Dataset):
 
-    def __init__(self, set_name="multi_train"):
+    def __init__(self, set_name="mini_train"):
         
+        #assert statements
         set_paths = ['train', 'val', 'test', 'mini_train', 'mini_val', 'train_detect', 'train_track']
         assert set_name in set_paths, "Incorrect set_name"
         
+        #Initialize data and Prediction Helper classes
         self.data_path = DATA_PATH
-        self.nusc = NuScenes(version='v1.0-mini', dataroot=self.data_path, verbose=True)
-#         self.trainset = get_prediction_challenge_split("mini_train", dataroot=data_path)
+        self.nusc = NuScenes(version=DATA_VERSION, dataroot=self.data_path, verbose=True)
         self.helper = PredictHelper(self.nusc)
     
         #get all the scenes
         self.scenes = create_splits_scenes()
+        
         #get all the scenes in the trainset
         self.set_name = set_name
         self.trainset = self.scenes[self.set_name] #List of scenes as part of training set
         self.prediction_scenes = json.load(open(self.data_path+"maps/prediction_scenes.json", "r")) #Dictionary containing list of instance and sample tokens for each scene
         
         print("Number of samples in train set: %d" % (len(self.trainset)))
-
 
     def __len__(self):
         return len(self.trainset) #return length of labels or input should be the same
@@ -66,20 +67,55 @@ class nuScenesdata(Dataset):
         scene = self.trainset[test_idx]
         
         #get all the tokens in the scene
-        scene_tokens = self.prediction_scenes[scene] #List of scene tokens in the given scene where each item comprises of an instance token and a sample token seperated by underscore
-        
+        #List of scene tokens in the given scene where each item comprises of an instance token and a sample token seperated by underscore
+        scene_tokens = self.prediction_scenes[scene] 
+      
+        #Return if fewer than 2 tokens in this scene
         if len(scene_tokens) < 2:
             print("Not enough agents in the scene")
             return []
         
         #get the tokens in the scene: we will be using the instance tokens as that is the agent in the scene
         tokens = [scene_tok.split("_") for scene_tok in scene_tokens]
-        instance_tokens, sample_tokens = list(list(zip(*tokens))[0]), list(list(zip(*tokens))[1]) #List of instance tokens and sample tokens
+        
+        #List of instance tokens and sample tokens
+        instance_tokens, sample_tokens = list(list(zip(*tokens))[0]), list(list(zip(*tokens))[1]) 
         
         assert len(instance_tokens) == len(sample_tokens), "Instance and Sample tokens count does not match"
         
-        token_count = Counter(instance_tokens) #Dictionary containing count for number of samples per token
-        minCount = sorted(list(token_count.values()), reverse=True)[NUM_AGENTS-1] #used to find n agents with highest number of sample_tokens
+        
+        '''
+        1. Convert list of sample and instance tokens into an ordered dict where sample tokens are the keys
+        2. Find n agents that have the most presence and store their instance tokens
+        3. Form a list of data points where each data point has TRAJECOTRY_TIME_INTERVAL sample tokens where 
+            each sample token has data for all instance tokens identified in step 2
+        4. Create 3 numy arrays each for coordinates, heading_change_rate and map with appropriate shapes
+        5. Iterate: per sample per instance and fill in numpy arrays with respective data
+        6. Form a dict containing the 3 numpyarrays and return it
+        '''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Dictionary containing count for number of samples per token
+        token_count = Counter(instance_tokens) 
+        
+        #used to find n agents with highest number of sample_tokens
+        minCount = sorted(list(token_count.values()), reverse=True)[NUM_AGENTS-1] 
     
         #Convert isntance and sample tokens to dict format
         instance_sample_tokens = {}
@@ -90,13 +126,9 @@ class nuScenesdata(Dataset):
                 except:
                     instance_sample_tokens[instance_token] = [sample_token]
                 
-        print("Instance:samples ===============================================================================")
-        print(instance_sample_tokens)
+#         print("Instance:samples ===============================================================================")
+#         print(instance_sample_tokens)
 
-#         #extract the sample and get all the data (agents) pertaining to that
-#         sample = self.helper.get_annotations_for_sample(sample_token)
-
-        print("Index number: ", test_idx)
         if len(list(instance_sample_tokens.keys())) != NUM_AGENTS:
             print()
 #             print("Instance_sample_tokens: \n", instance_sample_tokens)         
@@ -107,66 +139,60 @@ class nuScenesdata(Dataset):
         }
         '''
         
+        #Initialize map rasterizers
         static_layer_rasterizer = StaticLayerRasterizer(self.helper)
         agent_rasterizer = AgentBoxesWithFadedHistory(self.helper, seconds_of_history=2.5)
         mtp_input_representation = InputRepresentation(static_layer_rasterizer, agent_rasterizer, Rasterizer())
-        sliding_window = 5 #number of recordings to store per sample
         
-        output_data = {"coordinates": [0]*len(instance_sample_tokens.keys()), "heading_change_rate": [0]*len(instance_sample_tokens.keys()), "map":[0]*len(instance_sample_tokens.keys())}
+        #Initialize Output data
+        output_data = {"coordinates": np.zeros((len(instance_sample_tokens.keys()), 1)), "heading_change_rate": np.zeros((len(instance_sample_tokens.keys()), 1)), "map":[0]*len(instance_sample_tokens.keys())}
         
         for t,instance_token in enumerate(instance_sample_tokens.keys()):
-#             instance_token = instance_tokens[0]
-#             print("===============================================")
-            instance_coordinates = [[]]
-            instance_heading_change_rate = [[]]
-            idx = 0
-            num = 0
-            for sample_token in (instance_sample_tokens[instance_token]):
-#                 print(self.nusc.get('sample', sample_token)["timestamp"]) 
-#                 sample_token = instance_sample_tokens[instance_token][0] #Temporarily getting the first sample_token for a given instance_token
-#                 instance_time_interval = len(instance_sample_tokens[instance_token]) #Number of available sample_tokens for a given instance_token
 
-#                 #get all the future data for the sample
-#                 future_instance_global = self.helper.get_future_for_agent(instance_token,sample_token, \
-#                                                                           seconds=TRAJECTORY_TIME_INTERVAL, in_agent_frame = False)
+            instance_coordinates = np.zeros((int(len(instance_sample_tokens[instance_token]) / TRAJECTORY_TIME_INTERVAL),TRAJECTORY_TIME_INTERVAL,3))
+            instance_heading_change_rate = np.zeros((int(len(instance_sample_tokens[instance_token]) / TRAJECTORY_TIME_INTERVAL),TRAJECTORY_TIME_INTERVAL))
+            
+            print("Shape of instance_coordinates: ", instance_coordinates.shape)
+            idx = 0                       #0 --> numData points for this instance (dimension 1)
+            num = 0                       #0 --> TRAJECTORY_TIME_INTERVAL (dimension 2)
+            for sample_token in (instance_sample_tokens[instance_token]):
+#                 print(idx, "     ", num)
+#                 print(self.nusc.get('sample', sample_token)["timestamp"]) 
 
                 #how to get the annotation for the instance in the sample
                 annotation = self.helper.get_sample_annotation(instance_token, sample_token)
-                instance_coordinates[idx].append(annotation["translation"])
+                instance_coordinates[idx][num] = annotation["translation"]
 
                 #get the heading change rate of the agent
                 heading_change_rate = self.helper.get_heading_change_rate_for_agent(instance_token, sample_token)
-                instance_heading_change_rate[idx].append(heading_change_rate)
+                instance_heading_change_rate[idx][num] = heading_change_rate
+                
+                
                 num = num + 1
                 
                 #reached the number of records per sample
-                if num == sliding_window:
+                if num == TRAJECTORY_TIME_INTERVAL:
                     idx = idx+1
                     num = 0
-                    instance_coordinates.append([])
-                    instance_heading_change_rate.append([])
-                
-                
-                
-#                 anns = [ann for ann in self.nusc.sample_annotation if ann['instance_token'] == instance_token_img]
+                    
+                if idx == instance_coordinates.shape[0]:
+                    break
+                    
                 img = mtp_input_representation.make_input_representation(instance_token, sample_token)
-#                 print("I am printing jack shit ==============================================================================================")
-#                 print(img.shape)
 #                 cv2.imshow("map",img)
-                
-            
-#             print(np.array(instance_coordinates[0:len(instance_coordinates)-1]))
+
             output_data["map"][t]=(img)
-            plt.imsave('test'+str(test_idx)+str(t)+'.jpg',img)
-            output_data["coordinates"][t]=(instance_coordinates[0:len(instance_coordinates)-1])
-            output_data["heading_change_rate"][t]=(instance_heading_change_rate)
+#             plt.imsave('test'+str(test_idx)+str(t)+'.jpg',img)
+            output_data["coordinates"][t]=instance_coordinates
+            output_data["heading_change_rate"][t]=instance_heading_change_rate
 
 #         test = pd.DataFrame(output_data,columns=["coordinates", "heading_change_rate", "map"])
 #         test.to_csv('test'+str(test_idx)+'.csv')
 
-#         print("Printing Output data")
-#         print((output_data["coordinates"]))
-#         print(len(output_data["heading_change_rate"]))
+        print("Printing Output data")
+        print((output_data["coordinates"]))
+        print(len(output_data["heading_change_rate"]))
+        print(len(output_data["coordinates"]))
         
         return output_data
         
