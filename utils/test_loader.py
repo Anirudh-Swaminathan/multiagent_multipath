6a5bf285db6ff094a6f38cc3ba78f73e19bfde73
Sai <skomatin@ucsd.edu>
2020-06-09 00:22:13 -0700

    fixed git issues

--- utils/test_loader.py
+++ utils/test_loader.py
@@ -1,28 +1,33 @@
 import os
 import torch
+
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import cv2
-from datareader import nuScenesdata
-from nuscenes.map_expansion.map_api import NuScenesMap
+from datareader_toy import toyScenesdata
+# from nuscenes.map_expansion.map_api import NuScenesMap
 
 # CUDA for PyTorch
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
-torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.benchmark = True
 
 # Parameters
 params = {'batch_size': 1,
           'shuffle': True,
-          'drop_last': True,
+          'drop_last': False,
           'num_workers': 1}
 
 # set_name: {'train', 'val', 'test', 'mini_train', 'mini_val', 'train_detect', 'train_track'}
-dataset = nuScenesdata(set_name = "mini_train")
+dataset = toyScenesdata()
 dataloader = DataLoader(dataset, **params)
 
 for batch, data in enumerate(dataloader):
-#     print(batch, (data))
+    print(batch, len(data))
+    print((data["map"]).shape)
+    map = np.array(data["map"][0,:])
+    cv2.imshow("map",map)
+    cv2.waitKey(0)
     break
